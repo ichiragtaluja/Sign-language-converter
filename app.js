df5fb09f806d50243fa775dfa2e2f418118ec4c9
ichiragtaluja <Ichiragtaluja@gmail.com>
2022-12-27 15:29:39 +0530

    final touch added

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-var buttonTranslate = document.querySelector("#button-translate")
-console.log(buttonTranslate)
+var buttonTranslate = document.querySelector("#button-translate");
+//var languageAPI = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var languageAPI = "https://api.funtranslations.com/translate/dothraki.json"
+
+function errorHandler(error) {
+    console.log("Error");
+    alert("Something went wrong")
+}
+
+function whenClicked() {
+    var textInput = document.querySelector("#textarea")
+    var textOutput = document.querySelector("#output")
+    var newAPI = languageAPI + "?text=" + textInput.value;
+    console.log(newAPI)
+    fetch(newAPI)
+        .then(response => response.json())
+        .then(json => textOutput.innerText = (json.contents.translated))
+        .catch(errorHandler)
+
+}
+
+buttonTranslate.addEventListener("click", whenClicked)
